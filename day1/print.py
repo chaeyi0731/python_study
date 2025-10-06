d06d5441b384aa64f39307cccf8e1b288e9fa310
chaeyi0731 <chaeyi990731@gmail.com>
2025-10-06 19:51:17 +0900

    오타 수정 및 "" 감싸기

--- day1/print.py
+++ day1/print.py
@@ -1,6 +1,4 @@
-print(Hello Worid)
+print("Hello world")
 
-#문자열은 ""로 감싸지 않으면 실행 되지 않는다 
-#    print(Hello Worid)
-#          ^^^^^^^^^^^
-# SyntaxError: invalid syntax. Perhaps you forgot a comma? #
+# 문자열을 "" 감싸자 잘 출력 된다 
+# 출력 내용 Hello world
