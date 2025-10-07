6a2f0a4848cab0b2be45f2eac4b78961171b266a
chaeyi0731 <chaeyi990731@gmail.com>
2025-10-08 00:28:10 +0900

    slice 사용법

---
+++ day2/slice.py
@@ -0,0 +1,18 @@
+#문자열 슬라이싱
+#문자열 뒤 대괄호 안에 콜론(:) 기호를 사용해 인덱스 범위를 지정한다.
+
+# 문자열 [a:b] 인덱스가 a인 문자부터 인덱스가 (b-1)인 문자까지 선택
+# 문자열 [:b] 쳐음부터 인텍스가 (b-1)인 문자까지 선택
+#문자열[a:] 인덱스가 a인 문자부터 끝까지 선택
+#문자열[:] 문자열 전체를 선택
+
+print("Mint Chocolate")
+print("Mint Chocolate"[0:4])
+#인덱스 4인 Mint 까지만 출력이 된다.
+print("Mint Chocolate"[:4])
+#또한 굳이 0을 넣지 않아도 Mint가 출력이 된다.
+
+print("Mint Chocolate"[5:14])
+#인덱스 5부터 14까지인 Chocolate 만 출력이 된다.
+print("Mint Chocolate"[5:])
+#위와 같이 마지막 인덱스를 적지 않아도 자동으로 Chocolate 만 출력이 된다.
