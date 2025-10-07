080ab3d70bb7f1243105a8cfd33d9988d3da2392
chaeyi0731 <chaeyi990731@gmail.com>
2025-10-07 23:55:35 +0900

    plus 연산자사용

---
+++ day1/plus.py
@@ -0,0 +1,11 @@
+#문자열 연산 방법
+
+print("I like " + "chocolate")
+
+#+ 연산자 사용시 문자열을 더 할 수 있다. 
+#출력결과: like chocolate 
+
+print("i like "+"Mint "+"chocolate")
+
+#여러번 더하는 것도 가능
+#출력 결과 : i like Mint chocolate
