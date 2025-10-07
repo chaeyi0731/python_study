5e44dfdc72aad120c201f8099d0a64ca6fc6bed0
chaeyi0731 <chaeyi990731@gmail.com>
2025-10-08 00:07:01 +0900

    Index

---
+++ day1/Index.py
@@ -0,0 +1,16 @@
+#인덱스(index)는 문자열을 이루는 각 문자의 위치를 의미한다.
+
+print("Mint Chocolate"[0])
+print("Mint Chocolate"[1])
+print("Mint Chocolate"[2])
+print("Mint Chocolate"[3])
+print("Mint Chocolate"[4])
+print("Mint Chocolate"[5])
+
+#결과 
+#M
+#i
+#n
+#t
+
+#C
