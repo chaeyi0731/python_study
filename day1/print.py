737359044072445ec30e39bb1d582fda19e65b35
chaeyi0731 <chaeyi990731@gmail.com>
2025-10-07 23:11:45 +0900

    type 함수 사용

--- day1/print.py
+++ day1/print.py
@@ -12,3 +12,19 @@ print (1,2,3)
 
 #숫자열 나열시 , 사용 , 는 출력시 출력 되지 않는다.
 # 출력내용 1 2 3
+
+#"" 가 나오개 하려면 문자열로 하면 된다 
+print("1,2,3")
+
+# 출력 내용 "1,2,3"
+
+#문자열 숫자열 불리언은 타입 함수로 알 수 있다.
+
+print (type(1))
+print (type("Hello World"))
+print(type(True))
+
+#실행 결과
+# <class 'int'>
+# <class 'str'>
+#<class 'bool'> 
