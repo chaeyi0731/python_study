8a7d5c4c59cd643e3aba84551043ce43b3201a0a
chaeyi0731 <chaeyi990731@gmail.com>
2025-10-09 00:49:38 +0900

    비교연산자 사용해보기

--- day2/bool.py
+++ day2/bool.py
@@ -5,3 +5,16 @@ print(type(True))
 
 #결과 <class 'bool'> bool은 boolean 의 줄임말
 
+#비교 연산자를 사용하여 숫자형의 크기를 알 수 있다.
+#비교 연산의 결과는 불리언 (true) or (False)이다.
+
+print(100000 > 200000) #크다
+print(100000 < 200000) #작다
+print(100000 == 200000) #같다
+print(100000 != 200000) #같지않다
+
+# 결과 
+#False
+#True
+#False
+#True
